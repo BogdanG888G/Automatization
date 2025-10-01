54335bd1bf965bdc347b0972e4c7b433e7142b56
BogdanG888G <BogdanG888G@yandex.ru>
2025-10-01 18:05:03 +0300

    Удалил тестовый ДАГ :)

--- dags/lubit_slonov.py
+++
@@ -1,5 +0,0 @@
-from airflow import DAG
-from airflow.operators import PythonOperator
-from datetime import datetime
-
-with DAG(dag_id = 'lol', start_date = '24.09.2025', schedule_interval = '@daily', )
